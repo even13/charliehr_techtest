cec5205b91dd5c1ad140e01c340bfd88afe8c9e5
Eve Noirault <eve.noirault@live.fr>
2020-11-06 13:46:47 +0000

    test: added edge case for leap years

--- user.rb
+++ user.rb
@@ -11,25 +11,27 @@ class User
 
     # Returns an Integer representing the user’s current age
     def age
-        ...
+        
     end
 
     # Returns a Date object for the user's next birthday
     def next_birthday
-        ...
+        
     end
 end
 
 tests = [
- Date.new(1986, 1, 1),
- Date.new(1988, Date.today.month, Date.today.day),
- Date.new(1990, 12, 30),
+   Date.new(1986, 1, 1),
+   Date.new(1988, Date.today.month, Date.today.day),
+   Date.new(1990, 12, 30),
+   Date.new(1992, 2, 29),
 ]
 puts "====== ages ======"
 tests.each do |date|
- puts "#{date} => #{User.new("Test", date).age}"
+    puts "#{date} => #{User.new("Test", date).age}"
 end
+
 puts "====== birthdays ======"
 tests.each do |date|
- puts "#{date} => #{User.new("Test", date).next_birthday}"
+    puts "#{date} => #{User.new("Test", date).next_birthday}"
 end
